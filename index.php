fa43886a9e06ee776a93ba512b74e7b389644270
Agamemnon94 <94798272+agamemnon94@users.noreply.github.com>
2022-07-04 11:18:37 +0200

    Project base implamented

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
+
+require 'vendor/autoload.php';
+
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+
+$twig = new Environment($loader, [
+  'debug' => true,
+  'cache' => false //__DIR__.'cache'
+]);
+
+$twig->addExtension(new DebugExtension());
+
+$page = isset($_GET['page']) && !empty($_GET['page']) ? $_GET['page'] : 'home';
+
+switch ($page) {
+
+  case 'home':
+    echo $twig->render('home.twig', []);
+    break;
+  case 'contact':
+
+    echo $twig->render('contact.twig', []);
+    break;
+
+  default:
+    echo $twig->render('home.twig', []);
+    break;
+}
